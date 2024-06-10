48f2f133f1ed9596cc7005f2beab7ddc81ac6049
astagriomore <aljhonescabilla3@gmail.com>
2024-06-10 09:33:19 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,42 @@
+function Random() {
+    alert("You have selected the Random Orbital Sander. Click OK to proceed with your purchase.");
+}
+function Palm() {
+    alert("You have selected the Palm Detail Sander. Click OK to proceed with your purchase.");
+}
+function Drill() {
+    alert("You have selected the Industrial Impact Drill. Click OK to proceed with your purchase.");
+}
+function Stripp() {
+    alert("You have selected the Wire Stripper. Click OK to proceed with your purchase.");
+}
+
+function Switch() {
+    alert("You have selected the Water Pump Pliers Quick. Click OK to proceed with your purchase.");
+}
+
+function Sharp() {
+    alert("You have selected the Wire Stripper Multifunctional Sharp Cutting Quick Wire Stripper. Click OK to proceed with your purchase.");
+}
+function Planter() {
+    alert("You have selected the Stainless Steel Seedling Planter Tool Set. Click OK to proceed with your purchase.");
+}
+
+function Hollow() {
+    alert("You have selected the All-steel Hardened Hollow Hoe, Handheld Weeding Rake. Click OK to proceed with your purchase.");
+}
+
+function Auger() {
+    alert("You have selected the Garden Plant Auger Drill Bit. Click OK to proceed with your purchase.");
+}
+function Drive() {
+    alert("You have selected the S-Ks W4800 Labor Saving Truck Tire Wrench 1\" Drive. Click OK to proceed with your purchase.");
+}
+
+function Stanley() {
+    alert("You have selected the Stanley 86-477 Socket Wrench Set 27 pcs x 1/2 DR. (10-32mm). Click OK to proceed with your purchase.");
+}
+
+function Allen() {
+    alert("You have selected the Stanley Hexagonal Allen Key Wrench Set Long Series. Click OK to proceed with your purchase.");
+}
